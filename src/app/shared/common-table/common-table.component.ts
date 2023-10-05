33046dd97c63d53897927572e1be20d88d737f1f
rhythm-chicmic <rhythm.sharma@chicmic.co.in>
2023-10-05 17:23:26 +0530

    feat : Header Structure settings creation

--- src/app/shared/common-table/common-table.component.ts
+++ src/app/shared/common-table/common-table.component.ts
@@ -4,6 +4,10 @@ import { CustomerService } from 'src/app/customer.service';
 import { Customer,Representative } from 'src/app/customer';
 import { Product } from 'src/app/product';
 import { ProductService } from 'src/app/product.service';
+export interface City {
+  name: string,
+  code: string
+}
 
 @Component({
   selector: 'app-common-table',
@@ -11,26 +15,35 @@ import { ProductService } from 'src/app/product.service';
   styleUrls: ['./common-table.component.scss'],
   providers:[MessageService]
 })
+
 export class CommonTableComponent implements OnInit{
 
   products!: Product[];
-
-
   cols!: any[];
+  cities!: City[];
+  selectedCity!: City;
+  
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) { 
+    this.cities = [
+      {name: 'New York', code: 'NY'},
+      {name: 'Rome', code: 'RM'},
+      {name: 'London', code: 'LDN'},
+      {name: 'Istanbul', code: 'IST'},
+      {name: 'Paris', code: 'PRS'}
+  ];
+  }
 
   ngOnInit() {
       this.productService.getProductsSmall().then(data => this.products = data);
 
       this.cols = [
-          { field: 'code', header: 'Code',disableSort:true },
-          { field: 'name', header: 'Name',disableSort:false },
-          { field: 'category', header: 'Category',disableSort:false },
-          { field: 'quantity', header: 'Quantity',disableSort:false },
-          { field: 'dropdown', header: 'Dropdown',disableSort:true },
-
-          
+          { field: 'code', header: 'Code',disableSort:true ,type:'text'},
+          { field: 'name', header: 'Name',disableSort:false ,type:'text'},
+          { field: 'category', header: 'Category',disableSort:false ,type:'text'},
+          { field: 'quantity', header: 'Quantity',disableSort:false ,type:'text'},
+          { field: 'dropdown', header: 'Dropdown',disableSort:true ,type:'dropdown'},
+          {fields:'action', header:'Action', disableSort:true, options: ["user-edit","eye","trash"],modal:{deleteModal:true},type:'action'}
       ];
   }
 }
